f42ea445b01242c27cc5fdfa7ea8d327f88e6698
wandriansyah <129151107+wandriansyah@users.noreply.github.com>
2023-03-28 14:33:22 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+
+    <h1>KAMU BERHASIL!</h1>
+    <img alt="Vue logo" class="logo" src="https://i.pinimg.com/564x/ed/11/bb/ed11bb645b8769c969df45cbc0aa41e9.jpg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="kamu Berhasil!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
